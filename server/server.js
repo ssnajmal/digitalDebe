d34e25fc24ebf5a31ac61ef7922e6586e7a6a899
Shahrukh Shehzad <ssnajmal@gmail.com>
2020-10-31 02:23:52 +0300

    getting socket io working

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const path = require('path');
+const  http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'public');
 var app = express();
+var server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static('public'))
 
-var server = app.listen(process.env.PORT || 5000, function () {
+server.listen(process.env.PORT || 5000, function () {
   var port = server.address().port;
   console.log("Express is working on port " + port);
 });
