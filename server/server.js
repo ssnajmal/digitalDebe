5cd4089a1783352cf73471bed465d841fda3afef
Shahrukh Shehzad <ssnajmal@gmail.com>
2020-11-18 00:35:39 +0300

    trials

--- server/server.js
+++ server/server.js
@@ -3,38 +3,66 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require  ('./utils/message')
-const publicPath = path.join(__dirname,'public');
-var app = express();
-var server = http.createServer(app);
+const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/isRealString');
+const {Users} = require('./utils/users');
+
+const publicPath = path.join(__dirname, '/../public');
+const port = process.env.PORT || 3000
+let app = express();
+let server = http.createServer(app);
 let io = socketIO(server);
+let users = new Users();
 
-app.use(express.static('public'))
+app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+  console.log("A new user just connected");
 
-io.on('connection',(socket) => {
-    // connection (listener caller)
-    console.log("a new user just connected");
-    socket.emit('newMessage', generateMessage('Admin','Welcome to Digital Debe'));
-    socket.broadcast.emit('newMessage', generateMessage('Admin',"New user Joined"));
+  socket.on('join', (params, callback) => {
+    if(!isRealString(params.name) || !isRealString(params.room)){
+      return callback('Name and room are required');
+    }
 
-    //message listener caller
-    socket.on('createMessage',(message, callback) => {
-    console.log("created message", message)
-    io.emit('newMessage',generateMessage(message.from, message.text))
-    callback('MESSAGE CREATED');
-    });
+    socket.join(params.room);
+    users.removeUser(socket.id);
+    users.addUser(socket.id, params.name, params.room);
 
-    socket.on('createLocationMessage', (coords) =>{
-    io.emit('newMessage', generateMessage('Admin',"We got your Coordinates!! Hurray!"))
-    })
-    socket.on('disconnect',function(socket){
-    console.log("a user just disconnected")
-    });
+    io.to(params.room).emit('updateUsersList', users.getUserList(params.room));
+    socket.emit('newMessage', generateMessage('Admin', `Welocome to ${params.room}!`));
 
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', "New User Joined!"));
+
+    callback();
+  })
+
+  socket.on('createMessage', (message, callback) => {
+    let user = users.getUser(socket.id);
+
+    if(user && isRealString(message.text)){
+        io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    }
+    callback('This is the server:');
+  })
+
+  socket.on('createLocationMessage', (coords) => {
+    let user = users.getUser(socket.id);
+
+    if(user){
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.lat, coords.lng))
+    }
+  })
+
+  socket.on('disconnect', () => {
+    let user = users.removeUser(socket.id);
+
+    if(user){
+      io.to(user.room).emit('updateUsersList', users.getUserList(user.room));
+      io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left ${user.room} chat room.`))
+    }
+  });
 });
 
-server.listen(process.env.PORT || 5000, function () {
-    var port = server.address().port;
-    console.log("Express is working on port " + port);
-});
+server.listen(port, ()=>{
+  console.log(`Server is up on port ${port}`);
+})
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,6 +1,19 @@
-//MEssage generator for export
+const moment = require('moment');
+
 let generateMessage = (from, text) => {
-return {from, text, createdAt: new Date().getTime()
-        }
-      };
-module.exports= {generateMessage};
+  return {
+    from,
+    text,
+    createdAt: moment().valueOf()
+  };
+};
+
+let generateLocationMessage = (from, lat, lng) => {
+  return {
+    from,
+    url: `https://www.google.com/maps?q=${lat}, ${lng}`,
+    createdAt: moment().valueOf()
+  }
+}
+
+module.exports = {generateMessage, generateLocationMessage};
